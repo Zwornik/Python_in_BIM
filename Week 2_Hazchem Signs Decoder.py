756f161d763d10243f3d6e0a38da3126611fad06
Zwornik <91259612+Zwornik@users.noreply.github.com>
2022-01-12 14:11:02 +0100

    Snacks summary added

--- Week 2_Hazchem Signs Decoder.py
+++ Week 2_Hazchem Signs Decoder.py
@@ -1,25 +1,27 @@
 # Week 2 – Project 2 from the book.
 # HAZCHEM SIGNS DECODER - This program decodes EAC (Emergency Action Code) and display advise for fire fighters.
 
-fightWith = {'1': 'coarse spray', '2': 'fine spray', '3': 'foam', '4': 'dry agent'}
-safetyPrec = ['P', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z']
+fightWith = {'1': 'coarse spray', '2': 'fine spray', '3': 'foam', '4': 'dry agent'}  # first digit of EAC code
+safetyPrec = ['P', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z']   # Second letter of EAC code
 
 while True:
-    EAC = input("\nPlease type EAC code: ").upper()
-    if EAC[0] in fightWith and EAC[1] in safetyPrec and (len(EAC) == 3 and EAC[2] == 'E' or len(EAC) == 2):
+    EAC = input("\nPlease type EAC code: ").upper() # User input made case insensitive
+    if EAC[0] in fightWith and EAC[1] in safetyPrec and \
+            (len(EAC) == 3 and EAC[2] == 'E' or len(EAC) == 2): # check if input is an EAC
         print('\nFire fighting instruction for EAC:', EAC, '\n'
               '----------------------------')
-        print('Use', fightWith[EAC[0]])
+        print('Use', fightWith[EAC[0]])  # Takes message from dictionary for first letter
         print('----------------------------')
-        if EAC[1] in ('P', 'S', 'W', 'Y',):
-            print('Substance is prone to violent or explosive reaction.')
-        if EAC[1] in ('P', 'R', 'W', 'X',):
+        if EAC[1] in ('P', 'S', 'W', 'Y',):  # Check if 2nd letter indicates violent substance
+            print('Substance is prone to violent or explosive reaction.\n'
+                  '----------------------------')
+        if EAC[1] in ('P', 'R', 'W', 'X',):  # Check what kid of protection 2nd letter indicates
             print('Wear liquid-tight suit/chemical protection clothing.')
         else:
             print('Wear breathing apparatus & fire kit.\n'
                   '----------------------------')
-        if len(EAC) == 3 and EAC[2] == 'E':
+        if len(EAC) == 3 and EAC[2] == 'E':   # check if 3rd letter is there and if it is 'E'
             print('Public safety hazard. Warn people to stay indoors with doors & windows shut.\n'
                   '----------------------------')
     else:
-        print('This is not the EAC code. Please type again.',EAC)
+        print('\n', EAC, 'is not valid EAC code. Please type again.')
