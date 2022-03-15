801eaa49ae0e2c76586c7163a91a57b0d3984824
Zwornik <91259612+Zwornik@users.noreply.github.com>
2022-03-15 20:33:07 +0100

    in progress

--- Week 2_Hazchem Signs Decoder.py
+++ Week 2_Hazchem Signs Decoder.py
@@ -4,25 +4,29 @@
 fightWith = {'1': 'coarse spray', '2': 'fine spray', '3': 'foam', '4': 'dry agent'}  # first digit of EAC code
 safetyPrec = ['P', 'R', 'S', 'T', 'W', 'X', 'Y', 'Z']   # Second letter of EAC code
 
+def printLine():
+    print('-' * 38)
+
 while True:
     EAC = input("\nPlease type EAC code: ").upper() # User input made case insensitive
     if EAC[0] in fightWith and EAC[1] in safetyPrec and \
             (len(EAC) == 3 and EAC[2] == 'E' or len(EAC) == 2): # check if input is an EAC
         print('\nFire fighting instruction for EAC:', EAC )
-        print('-' * 38)
+        printLine()
         print('Use', fightWith[EAC[0]])  # Takes message from dictionary for first letter
         print('-' * 38)
         if EAC[1] in ('P', 'S', 'W', 'Y',):  # Check if 2nd letter indicates violent substance
             print('Substance is prone to violent or explosive reaction.')
-            print('-' * 38)
+            printLine()
         if EAC[1] in ('T', 'R', 'W', 'X',):  # Check what kid of protection 2nd letter indicates
             print('Wear liquid-tight suit/chemical protection clothing.')
+            printLine()
         else:
             print('Wear breathing apparatus & fire kit.n')
-            print('-' * 38)
+            printLine()
         if len(EAC) == 3 and EAC[2] == 'E':   # check if 3rd letter is there and if it is 'E'
             print('Public safety hazard. Warn people to stay indoors with doors & windows shut.')
-            print('-' * 38)
+            printLine()
     else:
         print('\n', EAC, 'is not valid EAC code. Please type again.')
 
